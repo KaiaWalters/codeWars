9b95a5078fc2c8b26108be9cb8cda7b1276d136d
Kaia Walters <kaia.cwalters@gmail.com>
2019-08-11 18:14:33 -0400

    Update and rename polishAlphabet.js to fixStringCase.js

--- polishAlphabet/polishAlphabet.js
+++
@@ -1,6 +0,0 @@
-function correctPolishLetters(string) {
-  var correct = {'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z'};
-  return string.replace(/[ąćęłńóśźż]/g, match => correct[match]);
-}//The match() method retrieves the result of matching a string against a regular expression.
-
-correctPolishLetters("Jędrzej Błądziński")
